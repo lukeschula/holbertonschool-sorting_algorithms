2887f30ffe20f8958e96126869d7fd1f5d92da3a
root <schulaluke@gmail.com>
2023-12-19 09:49:42 -0800

    added while loop to traverse the list and compare adjacent elements to determine if they are in the correct order

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,4 +12,28 @@ void insertion_sort_list(listint_t **list)
 	if (list == NULL || (*list)->next == NULL)
 		return;
 	ptr = *list;
+	while ( ptr->next)
+	{
+		if (ptr->n > temp->next->n)
+		{
+			if (ptr->next->next == NULL && ptr->prev == NULL)
+			{
+				swapping_prev_next(ptr);
+				*list = ptr->prev;
+			}
+			else if (ptr->prev == NULL)
+			{
+				swapping_prev(ptr);
+				*list = ptr->prev;
+			}
+			else if (ptr->next->next == NULL)
+			{
+				swapping_next(ptr);
+			else
+				swapping_none(ptr);
+			print_list(*list);
+
+			
+
+
 		
