93732e6a43401313c69d3373f61843349d2c51d7
root <schulaluke@gmail.com>
2023-12-19 12:37:02 -0800

    created function for partitioning the array and recursively sorting the subarrays

---
+++ 2-selection_sort.c
@@ -0,0 +1,17 @@
+#include "sort.h"
+/**
+ *
+ *
+ *
+ */
+void help_quick_sort(int *array, int l, int r, size_t size)
+{
+	int pvt;
+
+	if (l >= r || l < 0)
+		return;
+	pvt = part(array, l, r, size)
+	help_quick_sort(array, l, pvt -1, size);
+	help_quick_sort(array, pvt + 1, r, size);
+}
+
