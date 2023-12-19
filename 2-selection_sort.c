d51988768fdd4faa30ebce51a9002fdf3b701cf8
root <schulaluke@gmail.com>
2023-12-19 12:44:12 -0800

    added if statement to handle if array is less than 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -4,18 +4,10 @@
  *
  *
  */
-void help_quick_sort(int *array, int l, int r, size_t size)
+void selection_sort(int *array, size_t size)
 {
-	int pvt;
+	size_t a, b, m_vlue;
 
-	if (l >= r || l < 0)
+	if (size < 2)
 		return;
-	pvt = part(array, l, r, size)
-	help_quick_sort(array, l, pvt -1, size);
-	help_quick_sort(array, pvt + 1, r, size);
-}
-/**
- *
- *
- */
 
